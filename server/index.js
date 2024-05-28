d4d7e2561b1184697635a66bee39e692a88791f0
Sergey <sn.fruman@gmail.com>
2024-05-29 00:14:24 +0300

    get *

--- server/index.js
+++ server/index.js
@@ -26,26 +26,11 @@ app.listen(process.env.PORT || 3001, () => {
 app.use("/api/users",users_routes);
 
 
+// Have Node serve the files for our built React app
+// app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Have Node serve the files for our built React app
-// // app.use(express.static(path.resolve(__dirname, "./client/build")));
-// app.use(express.static(path.join(__dirname, "/client/build")));
-
-// // All other GET requests not handled before will return our React app
-// app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-// });
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});

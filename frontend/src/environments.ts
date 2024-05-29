5103b8d5f7e9a4f2767c138930def47caf5968a1
Sergey <sn.fruman@gmail.com>
2024-05-29 14:05:28 +0300

    url serverurl

--- frontend/src/environments.ts
+++ frontend/src/environments.ts
@@ -1,2 +1,2 @@
 // export const SERVERURL=`http://localhost:3001`
-export const SERVERURL=`https://final-project-htp7.onrender.com`
+// export const SERVERURL=`https://final-project-htp7.onrender.com`

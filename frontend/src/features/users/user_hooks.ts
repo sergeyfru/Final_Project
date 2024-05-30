e67b630d1029898376d48d3c88d7b1933666f5cb
Sergey <sn.fruman@gmail.com>
2024-05-30 14:51:03 +0300

    fixed hooks

--- frontend/src/features/users/user_hooks.ts
+++ frontend/src/features/users/user_hooks.ts
@@ -3,14 +3,17 @@ import { createSelector } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
 
 import { register } from "./user_slice";
-import { StoreDispatchType,User } from "../../types/type";
-import { useAppDispatch,useAppSelector } from "../../app/store";
+import { StoreDispatchType, User } from "../../types/type";
+import { useAppDispatch, useAppSelector } from "../../app/store";
 
-export const useRegister = ({ u_firstname, u_lastname, u_email, p_password }: User)=>{
-    // const dispatch = useDispatch()
-    return useCallback(()=>{
 
-        useAppDispatch(register({ u_firstname, u_lastname, u_email, p_password } : User) )
-    },[useAppDispatch])
-    
+
+
+export const useRegister = ({ u_firstname, u_lastname, u_email, p_password }: User) => {
+    const dispatch = useAppDispatch()
+    return useCallback(() => {
+
+        dispatch(register({ u_firstname, u_lastname, u_email, p_password }))
+
+    }, [dispatch])
 }
